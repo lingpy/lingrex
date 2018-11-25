561956eb4d9ef4b13dabfc4c71c18852347de9b9
lingulist <mattis.list@lingulist.de>
2018-11-25 14:04:23 +0100

    fixes #13

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+        use_setuptools()
+    from setuptools import setup
 
 setup(
         name='lingrex',
