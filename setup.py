03db1c4766c3d17559bd4a7f805959d5afb676b9
LinguList <mattis.list@posteo.de>
2020-03-24 14:16:14 +0100

    update version of lingrex

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
         ],
-        version='0.1.2',
+        version='0.1.3',
         packages=find_packages(where='src'),
         package_dir={'': 'src'},
         install_requires=['lingpy', 'sinopy'],
