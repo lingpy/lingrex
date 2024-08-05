ea8cea980f376fc20d273c084b49e478536a5f97
lingulist <mattis.list@lingulist.de>
2024-08-05 21:44:15 +0200

    release 1.4.2

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
-from setuptools import setup, find_packages
+"""
+Setup for LingRex
+"""
+from setuptools import setup
+setup()
 
-
-setup(
-        name='lingrex',
-        description="Linguistic reconstruction with LingPy",
-        long_description=open('README.md').read(),
-        long_description_content_type="text/markdown",
-        author='Johann-Mattis List and Robert Forkel',
-        url='https://github.com/lingpy/lingrex',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-        ],
-        version="1.4.2.dev0",
-        packages=find_packages(where='src'),
-        package_dir={'': 'src'},
-        install_requires=['lingpy>=2.6.10'],
-        extras_require={
-            "test": ["pytest", "coverage", "pytest-mock", "pytest-cov"],
-            "dev": ["wheel", "twine", "tox", "black", "flake8"],
-        },
-        keywords="historical linguistics, computational linguistics, computer-assisted language comparison"
-        )
